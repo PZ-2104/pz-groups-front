a48418f20516e59273905148a3e93da540bb5519
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-04-21 01:30:09 +0300

    began to create a section "our team," added section headers with students, created student cards, received data from the API, began to display cards with the received data

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,79 @@
-import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Card from "./components/student_card/student_card.jsx";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [students, setStudents] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch("https://pz-group-back.onrender.com/api/student/");
+				const data = await response.json();
+				if (response.ok) {
+					setStudents(data.data);
+				} else {
+					console.error("Error fetching students:", data.error);
+				}
+			} catch (error) {
+				console.error("Error fetching students", error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	const filterStudentsByRole = (role) => {
+		if (!Array.isArray(students) || students.length === 0) {
+			console.log("No students found");
+			return [];
+		}
+		return students.filter((student) => student.role.eng === role);
+	};
 
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer"></a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
+			<section id="our_team">
+				<h2 className="section_title">
+					Our <span className="red_word">team</span>{" "}
+				</h2>
+				<p className="cards_title">
+					Front-end and Back-end <span className="red_word">Developers</span>{" "}
 				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+				<div className="cards_container">
+					{students.length ? (
+						filterStudentsByRole("Front-End Developer").map((student) => (
+							<Card key={student._id} student={student} />
+						))
+					) : (
+						<p>No students found</p>
+					)}
+				</div>
+				<p className="cards_title">
+					Full Stack <span className="red_word">Developers</span>{" "}
+				</p>
+				<div className="cards_container">
+					{students.length ? (
+						filterStudentsByRole("Full Stack Developer").map((student) => (
+							<Card key={student._id} student={student} />
+						))
+					) : (
+						<p>No students found</p>
+					)}
+				</div>
+				<p className="cards_title">
+					Software <span className="red_word">Developers</span>{" "}
+				</p>
+				<div className="cards_container">
+					{students.length ? (
+						filterStudentsByRole("Software Developer").map((student) => (
+							<Card key={student._id} student={student} />
+						))
+					) : (
+						<p>No students found</p>
+					)}
+				</div>
+			</section>
 		</>
 	);
 }
---
+++ src/components/student_card/student_card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./student_card.module.css";
+const studentCard = ({ student }) => {
+	return (
+		<div className={styles.student_card}>
+			<img className={styles.student_card_image} src={student.image} alt="student_image" />
+			<p className={styles.student_card_fullname}>{student.fullName.eng}</p>
+			<p className={styles.student_card_group}>ПЗ-2104</p>
+			<p className={styles.student_card_role}>{student.role.eng}</p>
+		</div>
+	);
+};
+export default studentCard;
