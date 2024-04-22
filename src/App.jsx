b36bcbb7da289add6a3d75d724dabe4ee831f798
faithful-fox <omgcreater@gmail.com>
2024-04-23 00:10:32 +0300

    main page without adaptation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import 'styles/main.css';
-import FacultyIcon from 'assets/faculty_icon.png';
-import TeamPhoto1 from 'assets/team_photo_1.png';
-import TeamPhoto2 from 'assets/team_photo_2.png';
-import TeamPhoto3 from 'assets/team_photo_3.png';
+import React from "react";
+import "./styles/main.css";
+import FacultyIcon from "./assets/faculty_icon.png";
+import TeamPhoto1 from "./assets/team_photo_1.png";
+import TeamPhoto2 from "./assets/team_photo_2.png";
+import TeamPhoto3 from "./assets/team_photo_3.png";
 
 function App() {
 	return (
@@ -33,30 +33,62 @@ function App() {
 				<div className="title">
 					<h1>Hello, this is the site of PZ-2104</h1>
 				</div>
-				<p className="description">Our group of students at the university is characterized by a high level of enthusiasm and commitment to achieving high results in the field of information technology. We not only learn, but also actively apply our knowledge to create innovative solutions that contribute to the development of the university environment and enrich the learning experience.</p>
+				<p className="description">
+					Our group of students at the university is characterized by a high level of enthusiasm and
+					commitment to achieving high results in the field of information technology. We not only learn, but
+					also actively apply our knowledge to create innovative solutions that contribute to the development
+					of the university environment and enrich the learning experience.
+				</p>
 			</div>
 			<div className="about-team">
 				<h2 className="our-team">Our Team</h2>
 				<p className="team-slogan">It's a team because our work is play.</p>
 
 				<div className="text-photo">
-					<div className="team-photo-1">
-						<img src={TeamPhoto1} alt="Team Photo 1" />
+					<div className="text-photo-1">
+						<div className="team-text-1">
+							<p>
+								We support each other, we love what we do, and we’re really good at it. That’s why we’ve
+								been able to attract the best people in UX/UI design, motion graphics, mobile tech,
+								project project management, and quality assurance. It’s also why the size of our team
+								doubles years. But it’s not summer camp. Our staff are professionals who can and often
+								do work closely with our clients’ own developers. Delivering on time and on spec is just
+								a matter of course.
+							</p>
+						</div>
+						<div className="team-photo-1">
+							<img src={TeamPhoto1} alt="Team Photo 1"/>
+						</div>
 					</div>
-					<div className="team-text-1">
-						<p>We support each other, we love what we do, and we’re really good at it. That’s why we’ve been able to attract the best people in UX/UI design, motion graphics, mobile tech, project management, and quality assurance. It’s also why the size of our team doubles every two years. But it’s not summer camp. Our staff are professionals who can and often do work closely with our clients’ own developers. Delivering on time and on spec is just a matter of course.</p>
+					<div className="text-photo-2">
+						<div className="team-text-2">
+							<p>
+								We support each other, we love what we do, and we’re really good at it. That’s why we’ve
+								been able to attract the best people in UX/UI design, motion graphics, mobile tech,
+								project management, and quality assurance. It’s also why the size of our team doubles
+								every two years. But it’s not summer camp. Our staff are professionals who can and often
+								do work closely with our clients’ own developers. Delivering on time and on spec is just
+								a matter of course.
+							</p>
+						</div>
+						<div className="team-photo-2">
+							<img src={TeamPhoto2} alt="Team Photo 2"/>
+						</div>
 					</div>
-					<div className="team-photo-2">
-						<img src={TeamPhoto2} alt="Team Photo 2" />
-					</div>
-					<div className="team-text-2">
-						<p>We support each other, we love what we do, and we’re really good at it. That’s why we’ve been able to attract the best people in UX/UI design, motion graphics, mobile tech, project management, and quality assurance. It’s also why the size of our team doubles every two years. But it’s not summer camp. Our staff are professionals who can and often do work closely with our clients’ own developers. Delivering on time and on spec is just a matter of course.</p>
-					</div>
-					<div className="team-photo-3">
-						<img src={TeamPhoto3} alt="Team Photo 3" />
-					</div>
-					<div className="team-text-3">
-						<p>We support each other, we love what we do, and we’re really good at it. That’s why we’ve been able to attract the best people in UX/UI design, motion graphics, mobile tech, project management, and quality assurance. It’s also why the size of our team doubles every two years. But it’s not summer camp. Our staff are professionals who can and often do work closely with our clients’ own developers. Delivering on time and on spec is just a matter of course.</p>
+					<div className="text-photo-3">
+						<div className="team-text-3">
+							<p>
+								We support each other, we love what we do, and we’re really good at it. That’s why we’ve
+								been able to attract the best people in UX/UI design, motion graphics, mobile tech,
+								project management, and quality assurance. It’s also why the size of our team doubles
+								every two years. But it’s not summer camp. Our staff are professionals who can and often
+								do work closely with our clients’ own developers. Delivering on time and on spec is just
+								a matter of course.
+							</p>
+						</div>
+						<div className="team-photo-3">
+							<img src={TeamPhoto3} alt="Team Photo 3"/>
+						</div>
 					</div>
 				</div>
 			</div>
