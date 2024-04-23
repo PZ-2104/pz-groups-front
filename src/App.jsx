b12e7456d2bacfe2e5d9d28e4cf8da44dbc40eeb
LebedenkoAndrii <lebedenko1928@gmail.com>
2024-04-23 17:41:57 +0300

    added all students by roles, section adapted, corrected margins and paddings and card sizes, added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./components/student_card/student_card.jsx";
+import CardTitle from "./components/card_title/card_title.jsx";
 
 function App() {
 	const [students, setStudents] = useState([]);
@@ -25,10 +26,10 @@ function App() {
 
 	const filterStudentsByRole = (role) => {
 		if (!Array.isArray(students) || students.length === 0) {
-			console.log("No students found");
+			console.log("Students not found");
 			return [];
 		}
-		return students.filter((student) => student.role.eng === role);
+		return students.filter((student) => student.role.eng.includes(role));
 	};
 
 	return (
@@ -37,40 +38,104 @@ function App() {
 				<h2 className="section_title">
 					Our <span className="red_word">team</span>{" "}
 				</h2>
-				<p className="cards_title">
-					Front-end and Back-end <span className="red_word">Developers</span>{" "}
-				</p>
+
+				<CardTitle role="Front-end & Back-end" red_word="Developers" />
 				<div className="cards_container">
 					{students.length ? (
 						filterStudentsByRole("Front-End Developer").map((student) => (
 							<Card key={student._id} student={student} />
 						))
 					) : (
-						<p>No students found</p>
+						<p>Students not found</p>
 					)}
 				</div>
-				<p className="cards_title">
-					Full Stack <span className="red_word">Developers</span>{" "}
-				</p>
+
+				<CardTitle role="Full Stack" red_word="Developers" />
 				<div className="cards_container">
 					{students.length ? (
 						filterStudentsByRole("Full Stack Developer").map((student) => (
 							<Card key={student._id} student={student} />
 						))
 					) : (
-						<p>No students found</p>
+						<p>Students not found</p>
 					)}
 				</div>
-				<p className="cards_title">
-					Software <span className="red_word">Developers</span>{" "}
-				</p>
+
+				<CardTitle role="iOS" red_word="Developers" />
 				<div className="cards_container">
 					{students.length ? (
-						filterStudentsByRole("Software Developer").map((student) => (
+						filterStudentsByRole("iOS Developer").map((student) => (
 							<Card key={student._id} student={student} />
 						))
 					) : (
-						<p>No students found</p>
+						<p>Students not found</p>
+					)}
+				</div>
+
+				<CardTitle role="Software" red_word="Developers" />
+				<div className="cards_container">
+					{students.length ? (
+						filterStudentsByRole("Software").map((student) => <Card key={student._id} student={student} />)
+					) : (
+						<p>Students not found</p>
+					)}
+				</div>
+
+				<CardTitle role="Python" red_word="Developers" />
+				<div className="cards_container">
+					{students.length ? (
+						filterStudentsByRole("Python Developer").map((student) => (
+							<Card key={student._id} student={student} />
+						))
+					) : (
+						<p>Students not found</p>
+					)}
+				</div>
+
+				<CardTitle role="Graphic & Web" red_word="Designers" />
+				<div className="cards_container">
+					{students.length ? (
+						filterStudentsByRole("Graphic Designer").map((student) => (
+							<Card key={student._id} student={student} />
+						))
+					) : (
+						<p>Students not found</p>
+					)}
+					{students.length ? (
+						filterStudentsByRole("Web Designer").map((student) => (
+							<Card key={student._id} student={student} />
+						))
+					) : (
+						<p>Students not found</p>
+					)}
+				</div>
+
+				<CardTitle role="System" red_word="Administrator" />
+				<div className="cards_container">
+					{students.length ? (
+						filterStudentsByRole("System Administrator").map((student) => (
+							<Card key={student._id} student={student} />
+						))
+					) : (
+						<p>Students not found</p>
+					)}
+				</div>
+
+				<CardTitle role="Sales" red_word="Managers" />
+				<div className="cards_container">
+					{students.length ? (
+						filterStudentsByRole("Manager").map((student) => <Card key={student._id} student={student} />)
+					) : (
+						<p>Students not found</p>
+					)}
+				</div>
+
+				<CardTitle role="Team" red_word="Leads" />
+				<div className="cards_container">
+					{students.length ? (
+						filterStudentsByRole("Team Lead").map((student) => <Card key={student._id} student={student} />)
+					) : (
+						<p>Students not found</p>
 					)}
 				</div>
 			</section>
--- src/components/student_card/student_card.jsx
+++ src/components/student_card/student_card.jsx
@@ -5,7 +5,7 @@ const studentCard = ({ student }) => {
 		<div className={styles.student_card}>
 			<img className={styles.student_card_image} src={student.image} alt="student_image" />
 			<p className={styles.student_card_fullname}>{student.fullName.eng}</p>
-			<p className={styles.student_card_group}>ПЗ-2104</p>
+			<p className={styles.student_card_group}>PZ-2104</p>
 			<p className={styles.student_card_role}>{student.role.eng}</p>
 		</div>
 	);
